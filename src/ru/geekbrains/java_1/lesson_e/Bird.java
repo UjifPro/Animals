761aae285c08a4d925a722c2670fd32be46077dd
Гоша Карпачев <ujifpro@gmail.com>
2020-03-18 23:38:48 +0300

    zoo

--- src/ru/geekbrains/java_1/lesson_e/Bird.java
+++ src/ru/geekbrains/java_1/lesson_e/Bird.java
@@ -2,20 +2,20 @@ package ru.geekbrains.java_1.lesson_e;
 
 public class Bird extends Animal {
 
-        private static final int MAX_DISTANCE_RUN = 5;
-        private static final int MAX_DISTANCE_SWIT = 0;
-        private static final double MAX_HEIGHT = 1;
+    private static final int MAX_DISTANCE_RUN = 5;
+    private static final int MAX_DISTANCE_SWIT = 0;
+    private static final double MAX_HEIGHT = 1;
 
-        public Bird(String name){
-            super(name, MAX_DISTANCE_RUN, MAX_DISTANCE_SWIT, MAX_HEIGHT);
-        }
+    public Bird(String name){
+        super(name, MAX_DISTANCE_RUN, MAX_DISTANCE_SWIT, MAX_HEIGHT);
+    }
 
-        public Bird(String name, int maxDistanceRun, int maxDistanceSwit, double maxHeight){
-            super(name, maxDistanceRun, maxDistanceSwit, maxHeight);
-        }
+    public Bird(String name, int maxDistanceRun, int maxDistanceSwit, double maxHeight){
+        super(name, maxDistanceRun, maxDistanceSwit, maxHeight);
+    }
 
-        @Override
-        public boolean swim(int distance){
-            return false;
-        }
+    @Override
+    public boolean swim(int distance){
+        return false;
+    }
 }
